7b1e8d8feb2076473fe4db5ce34383e704ed0aed
Thakoor Chandan <thakoorchandan@gmail.com>
2021-11-06 17:17:57 +0530

    Achieved Query Object

--- src/Components/HomePage/WhereTo.jsx
+++ src/Components/HomePage/WhereTo.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import "../../Styles/WhereTo.css";
+import "../../Styles/RoomsGuestsButton.css";
 import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DatePicker from "@mui/lab/DatePicker";
+import { Dropdown } from "react-bootstrap";
 
 const whereTo = {
   query: "",
@@ -23,6 +25,49 @@ function WhereTo() {
   const [formData, setFormData] = useState(whereTo);
   const [checkIn, setCheckIn] = useState(null);
   const [checkOut, setCheckOut] = useState(null);
+
+  const [formValues, setFormValues] = useState([
+    { Room: 1, Adults: 1, Kids: 0 },
+  ]);
+
+  const [roomCount, setRoomCount] = useState(2);
+
+  let handleChangeRoomInput = (i, e) => {
+    let newFormValues = [...formValues];
+    newFormValues[i][e.target.name] = e.target.value;
+    setFormValues(newFormValues);
+  };
+
+  let addFormFields = () => {
+    setRoomCount(roomCount + 1);
+    setFormValues([...formValues, { Room: roomCount, Adults: 1, Kids: 0 }]);
+  };
+
+  let removeFormFields = (i) => {
+    let newFormValues = [...formValues];
+    newFormValues.splice(i, 1);
+    setFormValues(newFormValues);
+  };
+
+  function add(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    const no_Of_Rooms = formValues.length;
+    const no_Of_Guests = formValues.reduce((accumulator, item) => {
+      return accumulator + item.Adults;
+    }, 0);
+    let guests = add(no_Of_Guests.toString().split("").map(Number));
+    formData.no_of_rooms = no_Of_Rooms;
+    formData.no_of_guests = guests;
+    console.log(formData);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -93,7 +138,79 @@ function WhereTo() {
 
         {/* <!------------ Where To Buttons ? --------------> */}
         <div className="setRooms">
-          <button id="1room-1guest">1 Room, 1 Guest</button>
+          <Dropdown className="dropdown_room_menu">
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Rooms {formValues.length}, Guests
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <div className="Room_main_Container">
+                <form onSubmit={handleSubmit}>
+                  {formValues.map((element, index) => (
+                    <div className="Room_parent_container">
+                      <div className="child-contain-content" key={index}>
+                        <h4 name="Room" value={index}>
+                          Room {index + 1}
+                        </h4>
+                        <div className="adult-container">
+                          <label>Adults</label>
+                          <select
+                            className="selectOption"
+                            onChange={(e) => handleChangeRoomInput(index, e)}
+                            value={element.Adults || 1}
+                            name="Adults"
+                          >
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                          </select>
+                        </div>
+                        <div className="kid-container">
+                          <label>Kids</label>
+                          <select
+                            className="selectOption"
+                            onChange={(e) => handleChangeRoomInput(index, e)}
+                            value={element.Kids || 0}
+                            name="Kids"
+                          >
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                          </select>
+                        </div>
+                        {index ? (
+                          <button
+                            className="Room-remove-button"
+                            type="button"
+                            onClick={() => removeFormFields(index)}
+                          >
+                            Remove
+                          </button>
+                        ) : null}
+                      </div>
+                    </div>
+                  ))}
+                  <div className="Room-button-section">
+                    <button
+                      className="Room-add-button"
+                      disabled={roomCount === 10}
+                      type="button"
+                      onClick={() => addFormFields()}
+                    >
+                      Add
+                    </button>
+                    <button className="Room-add-button" type="submit">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </Dropdown.Menu>
+          </Dropdown>
+
           <button id="special-rates">Special Rates</button>
           <button id="find-a-hotel">Find a Hotel</button>
         </div>
